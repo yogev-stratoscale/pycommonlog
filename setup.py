612b9ad1ee8a9e37870a56ef7789d9826a563508
Yogev Cohen <yogev@stratoscale.com>
2015-06-30 18:46:35 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pycommonlog',
+      version='1.0',
+      description='common loggin library',
+      author='Stratoscale',
+      author_email='support@stratoscale.com',
+      url='https://github.com/stratoscale/pycommonlog',
+      packages=[],
+     )
